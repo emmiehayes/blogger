27709b0e2754fefca31dddb918bc1804c666ce8a
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-04 21:16:51 -0600

    update test to include full edit process

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -8,12 +8,16 @@ describe "user can edit an article" do
         article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
 
         visit article_path(article_1)
-        click_link "edit"
+        click_link 'edit'
+        fill_in 'article[title]', with: 'Title has been updated!'
+        fill_in 'article[body]',  with: 'Body has been updated!'
+        click_link 'update_article'
 
-        expect(current_path).to eq(edit_article_path)
-
-        expect(page).to have_content("Title 1")
-        expect(page).to have_content("Body 1")
+        expect(current_path).to eq(article_path(article_1))
+        expect(page).to_not have_content(article_1.title)
+        expect(page).to_not have_content(article_1.body)
+        expect(page).to have_content('Title has been updated!')
+        expect(page).to have_content('Body has been updated!')
       end
     end
   end
