6ff9c2d985b0adf89817c40b14dd2afd62ad536b
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-05 14:23:48 -0600

    add comment partial, render to show page, test for comments

--- spec/features/user_sees_single_item_show_spec.rb
+++ spec/features/user_sees_single_item_show_spec.rb
@@ -4,12 +4,18 @@ describe "user sees one article" do
 	describe "they link from the articles index" do
 		it "displays information for one article" do
       article = Article.create!(title: 'Title_1', body: 'Body_1')
+			comment_1 = article.comments.create!(author_name: 'Emmie', body: 'sample comment')
+			comment_2 = article.comments.create!(author_name: 'Michelle Obama', body: 'Girl Power!')
 
-      visit articles_path
+			visit articles_path
       click_link article.title
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+		  expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
 		end
 	end
 end
