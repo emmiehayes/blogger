af3de7f2649ed816973af0e309468774b38d128e
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-05 16:54:33 -0600

    add spec for tag and tagging

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,5 +4,8 @@ describe Article, type: :model do
   describe "validations" do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
+
+    it {should have_many(:comments)}
+    it {should have_many(:taggings)}
   end
 end
