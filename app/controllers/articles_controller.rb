a0389d856a633a71b897f72319e3809ec8f2f7a6
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-04 20:36:15 -0600

    adjust spec to match page, adjust destroy method to match spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,8 +19,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    Article.destroy(params[:id])
     redirect_to articles_path
   end
 
