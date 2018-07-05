493d82ee6a97eb7a808b1c092a2625c9eccf873e
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-05 09:02:46 -0600

    remove underscore, test passes

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -11,6 +11,7 @@ describe "user can edit an article" do
         click_link 'edit'
         fill_in 'article[title]', with: 'Title has been updated!'
         fill_in 'article[body]',  with: 'Body has been updated!'
+        click_button 'Update Article'
 
         expect(page).to have_content('Title has been updated!')
         expect(page).to have_content('Body has been updated!')
