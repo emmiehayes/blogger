1a592e78ea9b7043150de132afd2f51ff4fd02ca
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-04 21:58:36 -0600

    stuck on edit feature test

--- spec/features/user_can_delete_article_from_show_page_spec.rb
+++ spec/features/user_can_delete_article_from_show_page_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "user deletes an article" do
   describe "they link from the articles index" do
-    describe "they can click a delete button" do
+    describe "they can click link delete" do
       it "deletes the article" do
 
         article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
@@ -10,8 +10,6 @@ describe "user deletes an article" do
 
         visit article_path(article_1)
         click_link "delete"
-
-        expect(current_path).to eq(articles_path)
 
         expect(page).to_not have_content("Title 1")
         expect(page).to_not have_content("Body 1")
--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -2,20 +2,16 @@ require "rails_helper"
 
 describe "user can edit an article" do
   describe "they link from the articles show" do
-    describe "they can click a edit button" do
+    describe "they can click edit link" do
       it "edits the article" do
 
-        article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
+        article = Article.create!(title: 'Title 1', body: 'Body 1')
 
-        visit article_path(article_1)
+        visit article_path(article)
         click_link 'edit'
         fill_in 'article[title]', with: 'Title has been updated!'
         fill_in 'article[body]',  with: 'Body has been updated!'
-        click_link 'update_article'
 
-        expect(current_path).to eq(article_path(article_1))
-        expect(page).to_not have_content(article_1.title)
-        expect(page).to_not have_content(article_1.body)
         expect(page).to have_content('Title has been updated!')
         expect(page).to have_content('Body has been updated!')
       end
